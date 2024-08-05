c43d791bb3d49be07272b487ad10049b1ad7bcff
Kanishk Pachory <98859888+Kanishkpachory@users.noreply.github.com>
2024-08-06 00:11:34 +0530

    chapter 4

---
+++ PS_conditional_statement.py
@@ -0,0 +1,52 @@
+# a = int(input(" enter 1st number :"))
+# b = int(input(" enter 2nd number :"))
+# c = int(input(" enter 3rd number :"))
+# d = int(input(" enter 4th number :"))
+
+# if(a>b and a>c and a>d):
+#     print(a," is the greatest number ")
+
+# elif(b>a and b>c and b>d):
+#     print(b," is the greatest number ")
+
+# elif(c>b and c>a and c>d):
+#     print(c," is the greatest number ")
+
+# elif(d>b and d>c and d>a):
+#     print(d," is the greatest number ")
+
+# a = int(input("enter the marks of 1st subject : "))
+# b = int(input("enter the marks of 2nd subject : "))
+# c = int(input("enter the marks of 3rd subject : "))
+
+# total_percentage =(100*(a + b + c))/300
+
+# if(total_percentage>=40 and a>=33 and b>=33 and c>=33):
+#     print("you passed",total_percentage , a , b ,c)
+# else:
+#     print("you failed",total_percentage)
+
+# p1 = "make a lot of money"
+# p2 = "buy now"
+# p3 = "subscribe this"
+# p4 = "click this"
+
+# say = input("write here :")
+
+# if (p1 in say or p2 in say or p3 in say or p4 in say) :
+#     print("this is a spam")
+# else :
+#     print("you are human")
+
+# username = input("whats your usermane type here : ")
+# if (len(username)<10):
+#     print("your username has less than 10 charater")
+# else:
+#     print("your name is good")
+
+# l = ["apple" , "kanishk" , "banana" , "cat"]
+# take = input("what you want to search in your list :")
+# if (take in l):
+#     print("yes it is here !")
+# else:
+#     print("oops! not here")
---
+++ loops.py
@@ -0,0 +1,43 @@
+# l = ["kanishk" , 'parakrsam' , "papa" , "her" ,"mummy" , "boski"]
+# i = 0
+# while(i<(len(l))):
+#     print(l[i])
+#     i += 1
+
+# for i in range(100):
+#     print(i)
+
+# for i in range(0,100,4):
+#     print(i)
+
+# l = [ 1 , 2 , 3 , 3 , 4 , 5 ,6 ]
+
+# for i in l:
+#     print(i)
+# else : 
+#     print("im done")
+# t = ( 2 , 3 ,3 ,4 ,4 ,5 ,6, 7)
+# for i in t:
+#     print(i)
+
+# for i in range(10):
+#     print(i)
+#     if(i==5):
+#         break
+#      print(i)
+
+# for i in range(10) :
+#     # print(i)
+#     if(i==5):
+#         continue
+#     print(i)
+
+# for i in range(100):
+#      pass
+
+# i =0
+# while(i<=100):
+#     print(i)
+#     i += 1
+    
+    
