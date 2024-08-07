82599a9e09d850ef7675c69efabc5cd938ece7fc
Kanishk Pachory <98859888+Kanishkpachory@users.noreply.github.com>
2024-08-07 22:39:11 +0530

    Chapter 5

---
+++ PS_loops.py
@@ -0,0 +1,75 @@
+# n = int(input("enter a numbr :"))
+
+# for i in range(11):
+#     # print(n,"*",i,"=", n*i)
+#     print(f"{n} X {i} = {n*i}")
+
+#     i += 1
+
+# l = ["harry" , "soham" ,"sachine" ,"rahul"]
+
+
+# for name in l:
+#     if(name.startswith("s")):
+#         print(f"{name} , good afternoon !")
+    
+
+# n = int(input("enter a numbr :"))
+# i=0
+# while(i<n):
+#     # print(n,"*",i,"=", n*i)
+#     print(f"{n} X {i} = {n*i}")
+
+#     i += 1
+
+# n = int(input("enter a number :"))
+
+# for i in range(2,n):
+#     if (n%i == 0):
+#         print("oops not a prime numer ")
+#         break
+#     else :
+#         print("yes it is a prime number")
+#         break
+
+# n = int(input("enter a natural number : "))
+# i = 1
+# sum = 0
+# while(i<=n):
+#     sum += i
+#     i +=1
+
+# print(sum)
+
+# n = int(input("enter a number you want factorial of :"))
+# fac = 1
+# for i in range(1,n+1):
+#     fac = i*fac
+#     # print("factoral of the given number is :")
+# print("factoral of the given number is :",fac)
+
+# n = int(input("enter a number :"))
+# for i in range (1,n+1):
+#     print(" "*(n-i),end="")
+#     print("*"*(2*i-1),end="")
+#     print("")
+
+# n = int(input("enter a number :"))
+# for i in range (1,n+1):
+    
+#     print("*"*(i),end="")
+#     print("")
+
+# n = int(input("enter a number :"))
+# for i in range(1 , n+1) :
+#     if(i==1 or i==n):
+#         print("*"*n,end="")
+#     else:
+#         print("*",end="")
+#         print(" "*(n-2),end="" )
+#         print("*",end="")
+#     print("")
+
+# n = int(input("enter a number :"))
+# for i in range(1,n+1):
+#     print(f"{n} X {(n+1)-i} = {n*((n+1)-i)}")
---
+++ functions.py
@@ -0,0 +1,27 @@
+# def avg(name):
+#     a = 3
+#     b = 4
+#     c = 5
+
+#     avg = (a + b +c + name)/3
+#     # print(avg)
+#     return avg ,"this is the answer"
+
+# # name = 3
+# a = avg(name=3)
+# print(a)
+
+# def good(name , greet ="thank you"):
+#     print(f"good day , {name}   !! , {greet}")
+
+# good("kanishk")
+# good("parakram","shukriya")
+
+# reccursion problem
+# def factrioal(n):
+#     if(n==0 or n==1):
+#         return 1
+#     return n * factrioal(n-1)
+
+# n = int(input("enter a number :"))
+# print(f"Factorial of the give number {n} is : {factrioal(n)}")
